26b4d1921802c8c625605909ed8799e07b39f7e4
Fady Abd Elshahid <fady0118@gmail.com>
2024-09-18 16:30:52 +0300

    mobile home section update

--- docs/Js/site.js
+++ docs/Js/site.js
@@ -1,3 +1,36 @@
-document.addEventListener("DOMContentLoaded",function(){
-    
-})
+// document.addEventListener("DOMContentLoaded",function(){})
+
+/* =============== Show Menu =============== */
+const navMenu = document.getElementById("nav-menu"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close");
+
+if(navToggle){
+    navToggle.addEventListener('click',() =>{
+        navMenu.classList.add('show__menu')
+    })
+}
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show__menu')
+    })
+}
+/* =============== Remove Menu Mobile =============== */
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () =>{
+    const navMenu = document.getElementById("nav-menu")
+    navMenu.classList.remove('show__menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/* =============== Add Blur Header =============== */
+
+
+/* =============== Show Scroll Up =============== */
+
+
+/* =============== Scroll Sections Active Link =============== */
+
+
+/* =============== Scroll Reveal Animation =============== */
+
